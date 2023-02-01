1ae089020641ba035560151f6962b2a5be565c96
WellyBelly <89990168+WellyBelly@users.noreply.github.com>
2023-02-01 20:44:35 +0200

    Created simple Login UI w/out functionality

--- app/src/main/java/com/example/loginform/MainActivity.kt
+++ app/src/main/java/com/example/loginform/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.loginform
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.loginform.ui.theme.LoginFormTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +48,56 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LoginForm() {
+    var userName: String by remember { mutableStateOf("") }
+    var password: String by remember { mutableStateOf("") }
 
+    val focusManager = LocalFocusManager.current
+
+    Column(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.title),
+            color = MaterialTheme.colors.primary,
+            fontSize = 32.sp,
+            modifier = Modifier.padding(8.dp)
+        )
+        OutlinedTextField(
+            value = userName,
+            onValueChange = {userName = it},
+            label = { Text(text = stringResource(R.string.username))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email, imeAction = ImeAction.Next),
+            trailingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "Email Icon")},
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp)
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            label = { Text(text = stringResource(R.string.password))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = {focusManager.clearFocus()}),
+            trailingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "Lock Icon")},
+            visualTransformation = PasswordVisualTransformation(),
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp)
+        )
+        Button(
+            onClick = { /*LOGIN_FUNCTION*/},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.sumbit),
+                fontSize = 20.sp
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
